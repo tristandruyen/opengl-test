6011ca313d2c566d06c1733cc657339b22d3a641
tristandruyen <tristandruyen@vault81.de>
2021-07-07 09:52:00 +0200

    Setup buffer

--- main.cpp
+++ main.cpp
@@ -32,22 +32,26 @@ int main(void) {
   std::cout << "Glew Version: " << glewGetString(GLEW_VERSION) << std::endl;
   std::cout << "GL Version:   " << glGetString(GL_VERSION) << std::endl;
 
+  float positions[6] = {
+      -0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f, // simple triangle
+  };
+
+  //
+  // Create a buffer and give it data
+  unsigned int buffer;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, 6, &positions, GL_STATIC_DRAW);
+  //glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0);
+
   /* Loop until the user closes the window */
+
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glBegin(GL_TRIANGLES);
-
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex2f(-0.5f, -0.5f);
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex2f(0.0f, 0.5f);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2f(0.5f, -0.5f);
-
-    glEnd();
-
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawElements(mode, GLsizei count, GLenum type, const void *indices)
     /* Swap front and back buffers */
     glfwSwapBuffers(window);
 
