36787e2b0477fdca66c6242aa349b045c7beb7fe
tristandruyen <tristandruyen@vault81.de>
2021-07-07 09:52:00 +0200

    Change format & Add some functions

--- main.cpp
+++ main.cpp
@@ -7,46 +7,47 @@
 
 #include <iostream>
 
-int main(void) {
-    GLFWwindow* window;
+int initGlfw(GLFWwindow* window) {
+    if (!glfwInit()) return -1;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    return 0;
+}
 
-    /* Init GLEW */
+void initGlew() {
     GLenum err = glewInit();
     if (GLEW_OK != err) {
-        /* Problem: glewInit failed, something is seriously wrong. */
         std::cout << "Error !";
     }
     std::cout << "Glew Version: " << glewGetString(GLEW_VERSION) << std::endl;
     std::cout << "GL Version:   " << glGetString(GL_VERSION) << std::endl;
+}
+
+int main(void) {
+    GLFWwindow* window;
+    if (initGlfw(window) != 0) {
+        return -1;
+    };
+
+    initGlew();
 
     float positions[6] = {
         -0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f,  // simple triangle
     };
 
-    //
-    // Create a buffer and give it data
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6, positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2,
-                          (const char*)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);  // Unbind Buffer
 
     while (!glfwWindowShouldClose(window)) {
